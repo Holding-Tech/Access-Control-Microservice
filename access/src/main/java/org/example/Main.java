5291fd083e3b3c50812f7d895e10cdf06d5568e2
SibusisoColin5 <162052193+SibusisoColin5@users.noreply.github.com>
2025-06-07 21:29:47 +0200

    Start-up the application,added the application.properties file,persistence.xml
    Start-up the application,added the application.properties file,persistence.xml

--- access/src/main/java/org/example/Main.java
+++ access/src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@SpringBootConfiguration
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Main.class, args);
     }
 }
